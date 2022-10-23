7a7033c5178d48c632aa05472a3d785cc38fdd6b
Prashant <prashantco111@gmail.com>
2022-10-23 23:37:29 +0530

    Singleton Completed

---
+++ Singleton.js
@@ -0,0 +1,23 @@
+const Singleton = (function () {
+  let instance;
+
+  function createInstance() {
+    const object = new Object("I am the instance");
+    return object;
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
+  };
+})();
+
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
+
+console.log(instance1)
+console.log("Same instance? " + (instance1 === instance2));
